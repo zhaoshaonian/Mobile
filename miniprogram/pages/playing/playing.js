354342fc0e713a9f332c4201d87807caf7af1b15
zhao de <535900639@qq.com>
2018-11-03 17:52:54 +0800

    上传文件

---
+++ miniprogram/pages/playing/playing.js
@@ -0,0 +1,67 @@
+// pages/playing/playing.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src:'https://apd-4c05d03a149180deb5e202612453c52a.v.smtcdns.com/om.tc.qq.com/AhRsh9qoosj7Mj8zyQT_5RG1zqyr4nx2HSCOPsoKQODY/uwMRJfz-r5hgYaQXGdGnCwQsKf_dEsnq3UJztMoyPd8/b0663wwql2a.p712.1.mp4?sdtfrom=v1010&guid=783de5f85a1269d2e6e325a52bc68a82&vkey=67B5E3354AB06BC783FEC50849A670043A69A068C174C18AC4A6AEB6AC997E917CCE9DC915AB3A92081827249253E98D246E15903865BA8EEFE35F34933C6BFDE2E52D40A4D9F68A8EEE3387078E333516150C1122DD0CB55444C65920E4829DE9CAAE637BAD1456108FD84F93FC847113BE79E26D27E22E'
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})

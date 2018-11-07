335716dc3f99ca27b9492a4bb6a55fb195c5b20d
zhao de <535900639@qq.com>
2018-11-07 20:58:32 +0800

    改正

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,24 +3,8 @@
 var app = getApp()
 Page({
   data: {
-    scrollId : 'red',
-    isScrollX: true,
-    bannerList  : [
-      {
-        url :　"../../images/hu_1.png"
-      },{
-        url :  "../../images/huang_1.jpg"
-      },{
-        url :  "../../images/zhao_1.jpg"
-      }
-    ],
-    activeBannerIndex : 0,
+    motto: 'Hello World',
     userInfo: {}
-  },
-  search:function(event){
-    wx.navigateTo({
-      url:"../search/search"
-    });
   },
   //事件处理函数
   bindViewTap: function() {
@@ -28,27 +12,18 @@ Page({
       url: '../logs/logs'
     })
   },
-  bindChange : function(e){
-      this.setData({activeBannerIndex : e.detail.current});
-  },
   onLoad: function () {
-    wx.showToast({
-      title:"加载中...",
-      icon:"loading",
-      duration:10000,
-    });
-    wx.hideToast();
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
-
       //更新数据
       that.setData({
         userInfo:userInfo
       })
     })
-    console.log(app.globalData);
-    this.setData({'windowWidth' : app.globalData.windowWidth});
+    wx.playBackgroundAudio({
+        dataUrl: 'http://m2.music.126.net/Yev_vdcos-1_SX2qC9WUfw==/7856010580878729.mp3'
+    })
   }
 })
